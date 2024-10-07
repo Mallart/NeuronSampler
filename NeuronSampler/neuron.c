4b99038f6f79d9f39362f64dcf4ad3c759c7c286
Mallart <matheo.allart09@gmail.com>
2024-10-07 04:30:49 +0200

    added more functions for neurons

--- NeuronSampler/neuron.c
+++ NeuronSampler/neuron.c
@@ -21,7 +21,15 @@ void* array_append(void** array, uint64_t array_size, void* element)
 void* array_append_no_duplicate(void** array, uint64_t array_size, void* element)
 {
 	if (!array_exists(array, array_size, element))
-		array_append(array, array_size, element);
+		return array_append(array, array_size, element);
+	return array;
+}
+
+void array_remove(void** array, uint64_t array_size, void* element)
+{
+	for (uint64_t i = 0; i < array_size; ++i)
+		if (array[i] == element)
+			array[i] = 0;
 }
 
 bool array_exists(void** array, uint64_t array_size, void* element)
@@ -32,16 +40,71 @@ bool array_exists(void** array, uint64_t array_size, void* element)
 	return false;
 }
 
-void bind_child(NS_NEURON* parent, NS_NEURON* child)
+NS_SYNAPSE* create_synapse(NS_NEURON* parent, NS_NEURON* child)
 {
+	NS_SYNAPSE* synapse = malloc(sizeof(NS_SYNAPSE));
+	if (!synapse)
+		return 0;
+	synapse->parent = parent;
+	synapse->child = child;
+	
 	if (!array_exists(parent->children, parent->n_children, child))
 	{
-		parent->children = array_append(parent->children, parent->n_children, child);
+		parent->children = array_append(parent->children, parent->n_children, synapse);
 		parent->n_children++;
 	}
+	
 	if (!array_exists(child->parents, child->n_parents, parent))
 	{
-		child->parents = array_append(child->parents, child->n_parents, parent);
-		child->n_parents++;
+		child->parents = array_append(child->parents, child->n_parents, synapse);
+		parent->n_parents++;
 	}
+
+	return synapse;
+}
+
+void destroy_synapse(NS_SYNAPSE* synapse)
+{
+	NS_NEURON* parent = synapse->parent;
+	NS_NEURON* child = synapse->child;
+
+	if (!array_exists(parent->children, parent->n_children, child))
+	{
+		array_remove(parent->children, parent->n_children, synapse);
+		parent->n_children--;
+	}
+	
+	if (!array_exists(child->parents, child->n_parents, parent))
+	{
+		array_remove(child->parents, child->n_parents, synapse);
+		parent->n_parents--;
+	}
+
+	free(synapse);
+}
+
+NS_NEURON* create_neuron()
+{
+	NS_NEURON* neuron = malloc(sizeof(NS_NEURON));
+	if (!neuron)
+		return 0;
+	neuron->parents = malloc(sizeof(NS_SYNAPSE*));
+	neuron->children = malloc(sizeof(NS_SYNAPSE*));
+	neuron->n_parents = 0;
+	neuron->n_children = 0;
+	neuron->function = 0;
+	return neuron;
+}
+
+void init_neuron(NS_NEURON* neuron)
+{
+	if(neuron->parents)
+		free(neuron->parents);
+	if(neuron->children)
+		free(neuron->children);
+	neuron->parents = malloc(sizeof(NS_SYNAPSE*));
+	neuron->children = malloc(sizeof(NS_SYNAPSE*));
+	neuron->n_parents = 0;
+	neuron->n_children = 0;
+	neuron->function = 0;
 }
--- NeuronSampler/neuron.h
+++ NeuronSampler/neuron.h
@@ -1,25 +1,40 @@
 #pragma once
 #include "libs.h";
 
-typedef struct
+struct NS_NEURON;
+struct NS_SYNAPSE;
+
+typedef struct NS_SYNAPSE
+{
+	struct NS_NEURON* parent;
+	struct NS_NEURON* child;
+	// synapse's weight
+	float weight;
+} NS_SYNAPSE;
+
+typedef struct NS_NEURON
 {
 	uint64_t n_parents;
 	uint64_t n_children;
-	void* function;
-	struct NS_NEURON* parents;
-	struct NS_NEURON* children;
+	struct NS_SYNAPSE** parents;
+	struct NS_SYNAPSE** children;
+	float (*function)(float); // activation function
 } NS_NEURON;
 
 // appends an element on an array of pointers and returns the new array
 // WARNING: can destroy the old array if there's not enough free space to add an element
 void* array_append(void** array, uint64_t array_size, void* element);
 void* array_append_no_duplicate(void** array, uint64_t array_size, void* element);
+// replaces an element with 0.
+void array_remove(void** array, uint64_t array_size, void* element);
 // returns true if the specified element exist in array
 bool array_exists(void** array, uint64_t array_size, void* element);
 
+// creates a new synapse between two neurons (binds them automatically)
+NS_SYNAPSE* create_synapse(NS_NEURON* parent, NS_NEURON* child);
+// unbinds two neurons
+void destroy_synapse(NS_SYNAPSE* synapse);
 // creates a new neuron
 NS_NEURON* create_neuron();
-// initiates a blank neuron or reset an existing one.
+// reset an existing neuron.
 void init_neuron(NS_NEURON* neuron);
-// bind a child to his new parent if they're not bound already.
-void bind_child(NS_NEURON* parent, NS_NEURON* child);
